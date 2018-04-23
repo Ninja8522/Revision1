3cdade13729f5ac12ec9e38be08c1be9ba7efc66
CesarPerez <cesar_dany2009@hotmail.com>
2018-04-23 10:45:57 -0600

    Revision2

--- Examen2daUnidad/TestClass.cs
+++ Examen2daUnidad/TestClass.cs
@@ -19,5 +19,32 @@ namespace Examen2daUnidad
             var suma = x.suma(numeros);
             Assert.AreEqual(esperado, suma);
         }
+        [Test]
+        public void Promedio()
+        {
+            double[] numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            double esperado = 8.4;
+            Pruebas x = new Pruebas();
+            var promedio = x.promedio(numeros);
+            Assert.AreEqual(esperado, promedio);
+        }
+        [Test]
+        public void Moda()
+        {
+            double[] numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            double esperado = 9;
+            Pruebas x = new Pruebas();
+            var promedio = x.moda(numeros);
+            Assert.AreEqual(esperado, promedio);
+        }
+        [Test]
+        public void Medaina()
+        {
+            double[] numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            double esperado = 9;
+            Pruebas x = new Pruebas();
+            var promedio = x.mediana(numeros);
+            Assert.AreEqual(esperado, promedio);
+        }
     }
 }
