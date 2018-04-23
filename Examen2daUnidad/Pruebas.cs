77b6618726651b683305a9df9ebeb74502ff9d6e
CesarPerez <cesar_dany2009@hotmail.com>
2018-04-23 10:42:12 -0600

    Initial commit

---
+++ Examen2daUnidad/Pruebas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2daUnidad
+{
+    class Pruebas
+    {
+        public double suma(double[] numeros)
+        {
+            double suma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                suma = suma + numeros[i];
+            }
+            return suma;
+        }
+        public double promedio(double[] numeros)
+        {
+            double suma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                suma = suma + numeros[i];
+            }
+            return (suma / numeros.Length);
+        }
+        public double moda(double[] numeros)
+        {
+            double numero = 0;
+            double[] aux = new double[10];
+            int pos = 0;
+            double mayor = 0;
+            int posicionmayor = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                aux[i] = 0;
+            }
+            for (int x = 0; x < numeros.Length; x++)
+            {
+                numero = numeros[x];
+                pos = x;
+                for (int y = 0; y < numeros.Length; y++)
+                {
+                    if (numeros[y] == numero)
+                    {
+                        aux[pos]++;
+                    }
+                }
+            }
+            mayor = aux[0];
+            posicionmayor = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (aux[i] > mayor)
+                {
+                    posicionmayor = i;
+                    mayor = aux[i];
+                }
+            }
+            return numeros[posicionmayor];
+        }
+        public double mediana(double[] numeros)
+        {
+
+            return (numeros[numeros.Length / 2]);
+        }
+    }
+}
+
---
+++ Examen2daUnidad/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2daUnidad
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Suma()
+        {
+            double[] numeros = { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            double esperado = 84;
+            Pruebas x = new Pruebas();
+            var suma = x.suma(numeros);
+            Assert.AreEqual(esperado, suma);
+        }
+    }
+}
